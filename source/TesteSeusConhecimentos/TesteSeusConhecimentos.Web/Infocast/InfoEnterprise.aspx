0c2a5d16b62807f322d7162211a85b6954248950
Wilsonmarrtins <34444132+Wilsonmarrtins@users.noreply.github.com>
2017-12-11 10:39:24 -0200

    Add files via upload

---
+++ source/TesteSeusConhecimentos/TesteSeusConhecimentos.Web/Infocast/InfoEnterprise.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="InfoEnterprise.aspx.cs" Inherits="TesteSeusConhecimentos.Web.Infocast.InfoEnterprise" %>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="form-width">
+        <h2 id="formStatus2" runat="server">Nova Empresa</h2>
+        <div class="form-group">
+            <asp:Label ID="lbEndereco" runat="server" Text="Label">Endereço:</asp:Label>
+            <asp:TextBox ID="txtEndereco" CssClass="form-control" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <asp:Label ID="lbCidade" runat="server" Text="Label">Cidade:</asp:Label>
+            <asp:TextBox ID="txtCidade" CssClass="form-control" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group">
+            <asp:Label ID="lbEstado" runat="server" Text="Label">Estado:</asp:Label>
+            <asp:TextBox ID="txtEstado" CssClass="form-control" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group">
+            <asp:Label ID="lbCep" runat="server" Text="Label">Cep:</asp:Label>
+            <asp:TextBox ID="TxtCep" CssClass="form-control" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group">
+            <asp:Label ID="lbAtividadeEmpresa" runat="server" Text="Label">Atividade Empresa:</asp:Label>
+            <asp:TextBox ID="txtAtividadeEmpresa" CssClass="form-control" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group">
+            
+            <asp:Button ID="btncadastrar" runat="server" OnClick="btncadastrar_Click" Text="Cadastrar" />
+            
+        </div>
+    </div>
+</asp:Content>
---
+++ source/TesteSeusConhecimentos/TesteSeusConhecimentos.Web/Infocast/InfoEnterprise.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TesteSeusConhecimentos.Domain;
+using TesteSeusConhecimentos.Entities;
+using TesteSeusConhecimentos.Infra;
+
+namespace TesteSeusConhecimentos.Web.Infocast
+{
+    public partial class InfoEnterprise : System.Web.UI.Page
+    {
+        private IEnterpriseRepository EnterpriseRepository;
+
+        private int IdEnterprise
+        {
+            set { ViewState["IdEnterprise"] = value; }
+            get
+            {
+                if (ViewState["IdEnterprise"] != null)
+                    return Convert.ToInt32(ViewState["IdEnterprise"]);
+
+                return 0;
+            }
+        }
+        public InfoEnterprise()
+        {
+            this.EnterpriseRepository = new EnterpriseRepository();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SetViewStateUser();
+                UpdateForm();
+            }
+        }
+
+        private void SetViewStateUser()
+        {
+            if (Request.QueryString["id"] != null)
+                IdEnterprise = Convert.ToInt32(Request.QueryString["id"]);
+            else
+                IdEnterprise = 0;
+        }
+
+        private void UpdateForm()
+        {
+            Enterprise enterprise = this.EnterpriseRepository.GetById(IdEnterprise);
+
+            if (enterprise != null)
+            {
+                formStatus2.InnerText = "Editar Empresa";
+                txtEndereco.Text = enterprise.StreetAdress;
+                txtCidade.Text = enterprise.City;
+                txtEstado.Text = enterprise.State;
+                TxtCep.Text = enterprise.ZipCode;
+                txtAtividadeEmpresa.Text = enterprise.State;
+            }
+        }
+
+        protected void btncadastrar_Click(object sender, EventArgs e)
+        {
+            Enterprise Enterprise = new Enterprise(IdEnterprise, txtEndereco.Text, txtCidade.Text, txtEstado.Text, TxtCep.Text, txtAtividadeEmpresa.Text);
+            EnterpriseRepository.Save(Enterprise);
+
+            Response.Redirect("~/Infocast/Users.aspx");
+        }
+    }
+}
